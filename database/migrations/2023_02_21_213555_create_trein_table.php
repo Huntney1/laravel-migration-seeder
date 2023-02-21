4c1caf1a4b2bc02eceeb310cebaa5e7d46fb439b
Huntney1 <devilwhiteone@hotmail.it>
2023-02-22 00:19:48 +0100

    first commit

---
+++ database/migrations/2023_02_21_213555_create_trein_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trein', function (Blueprint $table) {
+            $table->id();
+            $table->string('azienda', 100);
+            $table->string('stazione_di_partenza', 45);
+            $table->string('stazione_di_arrivo', 45);
+            $table->string('orario_di_partenza', 10);
+            $table->string('orario_di_arrivo', 10);
+            $table->string('codice_treno', 15);
+            $table->smallInteger('numero_carrozze');
+            $table->boolean('treno_in_orario')->unsigned();
+            $table->boolean('treno_cancellato')->unsigned();
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trein');
+    }
+};
---
+++ database/seeders/TreinSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Trein;
+
+
+class TreinSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for($i=0; $i<=10; $i++){
+
+            $newTrein = new Trein();
+            $newTrein->azienda = $faker->company();
+            $newTrein->stazione_di_partenza = $faker->city();
+            $newTrein->stazione_di_arrivo = $faker->city();
+            $newTrein->orario_di_partenza = $faker->time();
+            $newTrein->orario_di_arrivo = $faker->time();
+            $newTrein->codice_treno = $faker->numerify('');
+            $newTrein->numero_carrozze = $faker->randomDigit();
+            $newTrein->treno_in_orario = $faker->randomElement([true, false]);
+            $newTrein->treno_cancellato = $faker->randomElement([true, false]);
+            $newTrein->save();
+        }
+    }
+}
